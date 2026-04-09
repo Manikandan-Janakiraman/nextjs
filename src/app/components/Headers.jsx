65c85da1590f722400f025ac6a9d98c927fcf118
unknown <manij472@gmail.com>
2026-04-09 21:10:44 +0530

    p

--- src/app/components/Headers.jsx
+++ src/app/components/Headers.jsx
@@ -4,7 +4,7 @@ export default function Headers() {
     return (
         <div className='flex flex-row justify-around h-20 items-center bg-black'>
             <div className='flex cols-3 text-white'>
-                <h1> Nature </h1>
+                <h1> Nature Life </h1>
             </div>
 
             <div className='flex cols-9 text-white justify-between gap-5'>
