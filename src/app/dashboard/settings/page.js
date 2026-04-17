d90edd9445bf371a6bccad95ad2d5e5c7a4799ac
unknown <manij472@gmail.com>
2026-04-17 23:22:44 +0530

    q

--- src/app/dashboard/settings/page.js
+++ src/app/dashboard/settings/page.js
@@ -57,15 +57,14 @@ export default function setting() {
                     <h1 className="font-bold text-5xl">Setting</h1>
                     <p>
                         Our platform is built to bring the warmth and authenticity of home-cooked foods to people’s everyday lives. 
-                        In a fast-paced world where time is limited, we aim to provide meals that feel just like they are made at home—fresh, 
-                        comforting, and full of care. We believe food is not just about eating, but about experience, 
+                        In a fast-paced world where time is limited, we aim to provide meals that feel just like they are made at home—fresh, comforting, and full of care. We believe food is not just about eating, but about experience, 
                         health, and connection.
                     </p>
                     <p>
                         We connect users with talented home chefs who are passionate about cooking and sharing 
                         their unique recipes. From traditional meals to modern healthy options, 
                         our platform offers a wide variety of dishes that cater to different tastes and lifestyles. 
-                        
+                        Whether it’s a simple lunch, a special dinner, or a fitness-focused meal plan, we make it easy to find the right food.
 
                     </p>
                     <p>
